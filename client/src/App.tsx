b320864cd74bc6da46d57f8380150d0fcb8db73a
bblkumar8-stack <bblkumar8@gmail.com>
2026-01-07 16:34:41 +0000

    fix: remove missing toaster import causing build failure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useRef } from "react";
 import { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { useAuth } from "@/hooks/use-auth";
 import { Navigation } from "@/components/Navigation";
@@ -110,7 +109,6 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
         <Router />
       </TooltipProvider>
     </QueryClientProvider>
